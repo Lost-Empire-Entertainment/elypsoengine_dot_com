08bad3faf6bfc04a0452024276957f660636b08a
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2024-05-09 23:18:04 +0300

    download button, website icon, restructure

---
+++ assets/js/download.js
@@ -0,0 +1,5 @@
+var link_EngineDownload = "https://github.com/Lost-Empire-Entertainment/Elypso-engine/releases/download/v0.0.13-prototype/Elypso.engine.0.0.13.7z";
+
+document.getElementById("btn_DownloadEngine").addEventListener("click", function () {
+    window.location.href = link_EngineDownload;
+});
