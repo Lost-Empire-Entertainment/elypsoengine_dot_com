d14eff95c678048c6230e1b19a8cd7394c2110f7
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2024-05-10 18:07:45 +0300

    added older versions page

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,6 +1,11 @@
 // Download engine
-var link_EngineDownload = "https://github.com/Lost-Empire-Entertainment/Elypso-engine/releases/download/v0.0.13-prototype/Elypso.engine.0.0.13.7z";
+var link_LatestDownload = "https://github.com/Lost-Empire-Entertainment/Elypso-engine/releases/download/v0.0.13-prototype/Elypso.engine.0.0.13.7z";
+var link_0_0_12 = "https://github.com/Lost-Empire-Entertainment/Elypso-engine/releases/download/v0.0.12-prototype/Elypso.engine.0.0.12.7z";
 
 document.getElementById("btn_DownloadEngine").addEventListener("click", function () {
-    window.location.href = link_EngineDownload;
+    window.location.href = link_LatestDownload;
+});
+
+document.getElementById("btn_Download0-0-12").addEventListener("click", function () {
+    window.location.href = link_0_0_12;
 });
