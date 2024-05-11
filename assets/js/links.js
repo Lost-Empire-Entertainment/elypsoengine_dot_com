f8dab932e523b30eca03fa2222337f82f845a61a
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2024-05-11 07:23:51 +0300

    fixing broken links and pages

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,23 +1,42 @@
 // Links to pages
+var dev_link_Page_Home = "file:///C:/Users/Arvuti/Documents/websites/elypsoengine_dot_com/index.html";
+var dev_link_Page_Docs = "file:///C:/Users/Arvuti/Documents/websites/elypsoengine_dot_com/pages/docs.html";
+
 var link_Page_Home = "https://elypsoengine.com";
 var link_Page_Docs = "https://elypsoengine.com/pages/docs.html";
-
-document.getElementById("btn_Home").addEventListener("click", function () {
-    window.location.href = link_Page_Home;
-});
-
-document.getElementById("btn_Docs").addEventListener("click", function () {
-    window.location.href = link_Page_Docs;
-});
 
 // Links to download engine versions
 var link_LatestDownload = "https://github.com/Lost-Empire-Entertainment/Elypso-engine/releases/download/v0.0.13-prototype/Elypso.engine.0.0.13.7z";
 var link_0_0_12 = "https://github.com/Lost-Empire-Entertainment/Elypso-engine/releases/download/v0.0.12-prototype/Elypso.engine.0.0.12.7z";
 
-document.getElementById("btn_DownloadEngine").addEventListener("click", function () {
-    window.location.href = link_LatestDownload;
-});
+window.onload = function () {
+    var btnHome = document.getElementById("btn_Home");
+    var btnDocs = document.getElementById("btn_Docs");
 
-document.getElementById("btn_Download0-0-12").addEventListener("click", function () {
-    window.location.href = link_0_0_12;
-});
+    if (btnHome) {
+        btnHome.addEventListener("click", function () {
+            window.location.href = link_Page_Home;
+        });
+    }
+
+    if (btnDocs) {
+        btnDocs.addEventListener("click", function () {
+            window.location.href = link_Page_Docs;
+        });
+    }
+
+    var btnDownloadEngine = document.getElementById("btn_DownloadEngine");
+    var btnDownload012 = document.getElementById("btn_Download0-0-12");
+
+    if (btnDownloadEngine) {
+        btnDownloadEngine.addEventListener("click", function () {
+            window.location.href = link_LatestDownload;
+        });
+    }
+
+    if (btnDownload012) {
+        btnDownload012.addEventListener("click", function () {
+            window.location.href = link_0_0_12;
+        });
+    }
+};
