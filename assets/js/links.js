3888024043a9b9f9a1634f65eb28643fe902fd5a
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2024-05-11 06:25:40 +0300

    added documents and older versions pages

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,4 +1,16 @@
-// Download engine
+// Links to pages
+var link_Page_Home = "https://elypsoengine.com";
+var link_Page_Docs = "https://elypsoengine.com/pages/docs.html";
+
+document.getElementById("btn_Home").addEventListener("click", function () {
+    window.location.href = link_Page_Home;
+});
+
+document.getElementById("btn_Docs").addEventListener("click", function () {
+    window.location.href = link_Page_Docs;
+});
+
+// Links to download engine versions
 var link_LatestDownload = "https://github.com/Lost-Empire-Entertainment/Elypso-engine/releases/download/v0.0.13-prototype/Elypso.engine.0.0.13.7z";
 var link_0_0_12 = "https://github.com/Lost-Empire-Entertainment/Elypso-engine/releases/download/v0.0.12-prototype/Elypso.engine.0.0.12.7z";
 
@@ -9,11 +21,3 @@ document.getElementById("btn_DownloadEngine").addEventListener("click", function
 document.getElementById("btn_Download0-0-12").addEventListener("click", function () {
     window.location.href = link_0_0_12;
 });
-
-window.onload = function () {
-    // Check if the current page is older_versions.html
-    if (window.location.pathname === 'pages/older_versions.html') {
-        // Redirect to the desired path without showing the filename
-        window.history.replaceState({}, document.title, 'older_versions');
-    }
-};
