e223c312eba5696b31e32445afbf7555b7583ae6
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2024-08-14 11:31:17 +0300

    pages are scrollable, added tutorials page

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,17 +1,19 @@
 // Links to pages
-var dev_link_Page_Home = "file:///C:/Users/Arvuti/Documents/websites/elypsoengine_dot_com/index.html";
-var dev_link_Page_Docs = "file:///C:/Users/Arvuti/Documents/websites/elypsoengine_dot_com/pages/docs.html";
+var dev_link_Page_Home = "file:///C:/Users/sande/Documents/_WEBSITE PROJECTS/elypsoengine_dot_com/index.html";
+var dev_link_Page_Docs = "file:///C:/Users/sande/Documents/_WEBSITE PROJECTS/elypsoengine_dot_com/pages/docs.html";
+var dev_link_Page_Tutorials = "file:///C:/Users/sande/Documents/_WEBSITE PROJECTS/elypsoengine_dot_com/pages/tutorials.html"
 
 var link_Page_Home = "https://elypsoengine.com";
 var link_Page_Docs = "https://elypsoengine.com/pages/docs.html";
+var link_Page_Tutorials = "https://elypsoengine.com/pages/tutorials.html";
 
 // Links to download engine versions
-var link_LatestDownload = "https://github.com/Lost-Empire-Entertainment/Elypso-engine/releases/download/v0.0.13-prototype/Elypso.engine.0.0.13.7z";
-var link_0_0_12 = "https://github.com/Lost-Empire-Entertainment/Elypso-engine/releases/download/v0.0.12-prototype/Elypso.engine.0.0.12.7z";
+var link_LatestDownload = "https://github.com/Lost-Empire-Entertainment/Elypso-engine/releases/download/v0.0.14-prototype/Elypso.engine.0.0.14.7z";
 
 window.onload = function () {
     var btnHome = document.getElementById("btn_Home");
     var btnDocs = document.getElementById("btn_Docs");
+    var btnTutorials = document.getElementById("btn_Tutorials");
 
     if (btnHome) {
         btnHome.addEventListener("click", function () {
@@ -25,18 +27,17 @@ window.onload = function () {
         });
     }
 
+    if (btnTutorials) {
+        btnTutorials.addEventListener("click", function () {
+            window.location.href = dev_link_Page_Tutorials;
+        });
+    }
+
     var btnDownloadEngine = document.getElementById("btn_DownloadEngine");
-    var btnDownload012 = document.getElementById("btn_Download0-0-12");
 
     if (btnDownloadEngine) {
         btnDownloadEngine.addEventListener("click", function () {
             window.location.href = link_LatestDownload;
-        });
-    }
-
-    if (btnDownload012) {
-        btnDownload012.addEventListener("click", function () {
-            window.location.href = link_0_0_12;
         });
     }
 };
